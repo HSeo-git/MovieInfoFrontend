ab4b56da7e100894b0f7a1f7a1b6c41d8d542834
Justin Na <nawonjae@naver.com>
2022-04-23 19:18:36 +0900

    feat(footer): add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='footer'>
+            <div className='footer__copyright'>
+                &copy; 2021 Made by
+                <a
+                    className='footer__copyright--link'
+                    href='https://github.com/HSeo-git'
+                >
+                    {' '}
+                    Hagom
+                </a>
+            </div>
+        </footer>
     );
 };
 
