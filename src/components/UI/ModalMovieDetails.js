530824ea0dc9d0f351f613313b963139a44cd089
HG.Seo <sophieseo3@gmail.com>
2022-04-24 16:29:05 +0900

    feat(search): Add search page
    When adding search page, some hooks(useDebounce) and actions were needed.
    Therefore, some documents supporting the search page were made including the above.
    
    P.S. As the concept was changed from the game-lesson platform into the movie review,
    some parts in the menu of Navbar, hooks and components' contents were changed accordingly.
    The Logo was changed as well.
    In addition, the year on the footer was changed from 2021 into 2022.

---
+++ src/components/UI/ModalMovieDetails.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import {ReactComponent as AddIcon} from '../../static/images/icon-add.svg'
+import {ReactComponent as PlayIcon} from "../../static/images/icon-play.svg";
+
+const MovieDetails = ({
+                          movie: {
+                              title,
+                              name,
+                              vote_average,
+                              release_date,
+                              overview,
+                          },
+                      }) => {
+    return (
+        <div className='modal__container'>
+            <h1 className='modal__title'>{title || name}</h1>
+            <p className='modal__info'>
+                <span className='modal__rating'>Rating: {vote_average * 10}% </span>
+            </p>
+            <p className='modal__overview'>{overview}</p>
+            <button className='modal__btn modal__btn--red'>
+                <PlayIcon className='modal__btn--icon' />
+                Play
+            </button>
+            <button className='modal__btn'>
+                <AddIcon className='modal__btn--icon' />
+                My List
+            </button>
+        </div>
+    )
+}
+
+export default MovieDetails
